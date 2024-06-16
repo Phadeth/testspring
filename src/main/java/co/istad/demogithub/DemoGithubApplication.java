921cf9f35aa8ee8e0edcf01db608d6867eac813c
unknown <phum.2021phadeth@gmail.com>
2024-06-16 14:18:28 +0700

    SpringProject

--- src/main/java/co/istad/demogithub/DemoGithubApplication.java
+++ src/main/java/co/istad/demogithub/DemoGithubApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoGithubApplication {
 
     public static void main(String[] args) {
+        System.out.println("Spring Testing");
         SpringApplication.run(DemoGithubApplication.class, args);
     }
 
